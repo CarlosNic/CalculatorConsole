904a5168d9353aacbe36e7b7a74b540fccc017d5
CarlosNic <carlosdnicolau@outlook.com>
2024-01-10 21:17:34 -0300

    Create a Calculator Class

---
+++ CalculatorConsoleApp/CalculatorConsoleApp/CalculatorClass.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorConsoleApp
+{
+    public class CalculatorClass
+	{
+		public CalculatorClass()
+		{
+		}
+
+		public string Calculate(string str)
+		{
+			return str;
+		}
+	}
+}
+
--- CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -3,5 +3,9 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        CalculatorConsoleApp.CalculatorClass calculatorClass = new CalculatorConsoleApp.CalculatorClass();
+        string str = calculatorClass.Calculate("teste");
+        Console.WriteLine(str);
     }
 }
